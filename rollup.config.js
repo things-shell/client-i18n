d03b8cf8b943343f622e56446156f33e3a2e8bc4
Hearty Oh <heartyoh@hatiolab.com>
2019-02-19 19:27:30 +0900

    v0.2.0
    rollup

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+
+let pkg = require('./package.json')
+let external = Object.keys(pkg.dependencies)
+let plugins = [resolve(), babel({ exclude: 'node_modules/**' }), commonjs(), terser()]
+
+export default [
+  {
+    external,
+    input: 'src/index.js',
+    plugins,
+    output: [
+      {
+        file: 'dist/client-i18n.js',
+        name: 'auth',
+        format: 'umd',
+        sourcemap: true
+      }
+    ]
+  },
+  {
+    external,
+    input: 'src/index.js',
+    plugins,
+    output: [
+      {
+        file: pkg.module,
+        format: 'esm',
+        sourcemap: true
+      }
+    ]
+  }
+]
